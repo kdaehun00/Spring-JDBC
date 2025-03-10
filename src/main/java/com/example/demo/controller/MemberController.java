9cb92b534ed6f9a9d8bdd972470166dcc9c6b9cf
kdaehun00 <kdaehun00@naver.com>
2025-03-10 17:21:50 +0900

    Upadte

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
